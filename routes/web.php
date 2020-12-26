45b14169929dce3f8a0683a84f12db7f86b68076
Agung Gunawan <agunggunawan.github@gmail.com>
2020-12-26 15:52:01 +0700

    gunakan login dari fortify

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::post('login', App\Http\Controllers\LoginController::class)->name('login.store');
+// Route::post('login', App\Http\Controllers\LoginController::class)->name('login.store');
 Route::post('register', App\Http\Controllers\RegisterController::class)->name('register.store');
 Route::get('fallback', App\Http\Controllers\FallbackController::class)->name('fallback');
 Route::get('/', App\Http\Controllers\FallbackController::class);
